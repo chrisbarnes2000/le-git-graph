c9fac427a9ad06cd1e5787a6ebd9091a47f1a53e
Christopher Barnes <chris.barnes.2000@me.com>
2022-11-09 17:34:37 +0000

    [Refactor]: Firebase Service Worker

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,39 +1,38 @@
 try {
   self.importScripts("dependencies/firebase-app.js", "dependencies/firebase-database.js");
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyAfYmATG88Dsjhe2f-Q8YrMVW1ZRvY6azA",
-    authDomain: "github-tree-graph.firebaseapp.com",
-    databaseURL: "https://github-tree-graph-default-rtdb.firebaseio.com",
-    projectId: "github-tree-graph",
-    storageBucket: "github-tree-graph.appspot.com",
-    messagingSenderId: "258623901486",
-    appId: "1:258623901486:web:b41cb523dbb8ee6e9674bf",
-    measurementId: "G-WN4EFGB84W",
-  };
-  const app = firebase.initializeApp(firebaseConfig);
-  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    var browserToken = request.browserToken;
+  const app = firebase.initializeApp(
+    (firebaseConfig = {
+      apiKey: "AIzaSyAfYmATG88Dsjhe2f-Q8YrMVW1ZRvY6azA",
+      authDomain: "github-tree-graph.firebaseapp.com",
+      databaseURL: "https://github-tree-graph-default-rtdb.firebaseio.com",
+      projectId: "github-tree-graph",
+      storageBucket: "github-tree-graph.appspot.com",
+      messagingSenderId: "258623901486",
+      appId: "1:258623901486:web:b41cb523dbb8ee6e9674bf",
+      measurementId: "G-WN4EFGB84W",
+    }),
+  );
+
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const githubTokenStream = firebase.database().ref("TokenData/" + browserToken + "/githubToken");
-    chrome.tabs.sendMessage(sender.tab.id, { status: "LISTENING FOR " + "TokenData/" + browserToken + "/githubToken" });
+    const currentTab = (query) => chrome.tabs.query({ active: true, currentWindow: true }, query);
+    const tabMessenger = (message) => chrome.tabs.sendMessage(sender.tab.id, message);
+    var browserToken = request.browserToken;
+
+    tabMessenger({ status: "LISTENING FOR " + `TokenData/${browserToken}/githubToken` });
     githubTokenStream.on("value", (snapshot) => {
       var githubToken = snapshot.val();
+      if (githubToken == null) currentTab(() => tabMessenger({ status: "NULL" }));
       if (githubToken == "FAIL") {
-        chrome.tabs.sendMessage(sender.tab.id, { status: "FAIL" });
+        tabMessenger({ status: "FAIL" });
         githubTokenStream.off();
-      }
-      if (githubToken == null) {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-          chrome.tabs.sendMessage(sender.tab.id, { status: "NULL" });
-        });
       } else {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-          chrome.tabs.sendMessage(sender.tab.id, { status: "SUCCESS", value: githubToken });
-        });
+        currentTab(() => tabMessenger({ status: "SUCCESS", value: githubToken }));
         githubTokenStream.off();
       }
     });
   });
-} catch (e) {
-  console.log(e);
+} catch (error) {
+  console.log(`Loading Firebase Error: ${error}`);
 }
